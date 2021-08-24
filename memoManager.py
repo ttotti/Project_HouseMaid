540719b56bc1813f00f869968f5cae781f03b0c3
ttotti <prs12344@naver.com>
2021-08-25 02:25:36 +0900

    2021-08-25

--- memoManager.py
+++ memoManager.py
@@ -9,16 +9,20 @@ class MemoManager(QWidget):
         super(MemoManager, self).__init__(parent)
         self.parent = parent
         self.folderText = ""
+        self.file_list = list()
 
         print("MemoManger().__init__()")
         
         # 레이아웃 배치
+        # 스텍 위젯 - 여러 위젯을 한 공간에 저장해서 선택할 수 있게 한다.
+        self.stackedwidget = QStackedWidget()
+        self.stackedwidget.addWidget(File_Widgit().widget)
+
         self.Hboxlayout = QHBoxLayout()
         self.Hboxlayout_Left = QHBoxLayout()
         self.Vboxlayout_Right = QVBoxLayout()
 
         self.folder_treewidget = QTreeWidget()
-        self.file_treewidget = QTreeWidget()
 
         # 마우스 트래킹
         self.folder_treewidget.setMouseTracking(True)
@@ -29,7 +33,6 @@ class MemoManager(QWidget):
         self.textEdit.setAcceptRichText(False)
 
         self.folder_root = QTreeWidget.invisibleRootItem(self.folder_treewidget)
-        self.file_root = QTreeWidget.invisibleRootItem(self.file_treewidget)
 
         #self.folder_treewidget.itemClicked.connect(self.clickItem)
         self.folder_treewidget.itemPressed.connect(self.folder_pressedItem)
@@ -46,27 +49,21 @@ class MemoManager(QWidget):
         self.folder_treewidget.setHeaderLabel("목록")
         self.folder_treewidget.setContextMenuPolicy(Qt.ActionsContextMenu)
 
-        self.file_treewidget.setHeaderLabels(["제목", "생성일", "수정일", "그룹"])
-        self.file_treewidget.setContextMenuPolicy(Qt.ActionsContextMenu)
-        self.file_treewidget.setColumnWidth(0, 160)
-        self.file_treewidget.setColumnWidth(1, 150)
-        self.file_treewidget.setColumnWidth(2, 150)
-
         self.Hboxlayout.addLayout(self.Hboxlayout_Left)
         self.Hboxlayout.setSpacing(1)
 
         H_splitter = QSplitter(Qt.Horizontal)
-        V_splitter = QSplitter(Qt.Vertical)
+        self.V_splitter = QSplitter(Qt.Vertical)
 
-        V_splitter.addWidget(self.file_treewidget)
-        V_splitter.addWidget(self.textEdit)
+        self.V_splitter.addWidget(self.stackedwidget)
+        self.V_splitter.addWidget(self.textEdit)
 
         H_splitter.addWidget(self.folder_treewidget)
-        H_splitter.addWidget(V_splitter)
+        H_splitter.addWidget(self.V_splitter)
 
         H_splitter.setStretchFactor(1, 3)
         H_splitter.setStretchFactor(0, 1)
-        V_splitter.setStretchFactor(1, 1)
+        self.V_splitter.setStretchFactor(1, 1)
         
         self.Hboxlayout_Left.addWidget(H_splitter)
 
@@ -85,16 +82,10 @@ class MemoManager(QWidget):
         # self.folder_treewidget.addAction(self.foldertree_newfile)
         # self.foldertree_newfile.setEnabled(False)
 
-        self.newfile = QAction("새 파일", self.file_treewidget)
-        self.file_treewidget.addAction(self.newfile)
-        self.newfile.setEnabled(False)
-
         # self는 실제로 bool 변수가 된다 -> 파이썬 규칙
         # 함수를 클래스 외부에 놓거나 인수를 추가할때는 lambda를 사용
         self.newfolder.triggered.connect(lambda : self.folder_contextClick())
         self.removefolder.triggered.connect(lambda : self.removefolder_contextClick())
-        self.newfile.triggered.connect(lambda : self.file_contextClick())
-
 
     # 폴더추가
     def addFolder(self, title):
@@ -106,23 +97,20 @@ class MemoManager(QWidget):
 
         return item
 
-    # 파일추가
-    def addFile(self, title, date, modify, group):
-        # 트리위젯에 추가할 아이템 생성
-        item = QTreeWidgetItem()
-
-        item.setText(0, title)
-        item.setText(1, date)
-        item.setText(2, modify)
-        item.setText(3, group)
-
-        return item
 
     def folder_contextClick(self):
         print("newfolder.triggered.connect()")
 
+        # 폴더 생성 시 리스트에 파일 위젯 추가
+        self.file_list.append(File_Widgit())
+        self.listIndex = len(self.file_list)
+        print("list : {0}".format(self.listIndex))
+
+        self.stackedwidget.addWidget(self.file_list[self.listIndex-1].widget)
+
         self.folder_root.addChild(self.addFolder("bird 폴더"))
         # print(self.folder_root.childCount())
+
 
     def removefolder_contextClick(self):
         print("removefolder.triggered.connect()")
@@ -130,14 +118,6 @@ class MemoManager(QWidget):
         self.folder_root.removeChild(self.folder_item)
         
         # print(self.folder_root.childCount())
-
-    def file_contextClick(self):
-        print("newfile.triggered.connect()")
-
-        day = QDate.currentDate()
-        time = QTime.currentTime()
-
-        self.file_root.addChild(self.addFile("bird 파일", day.toString('yyyy/MM/dd') + " - " + time.toString('hh:mm:ss'), day.toString('yyyy/MM/dd')+ " - " + time.toString('hh:mm:ss'), self.folderText))
 
     # def clickItem(self, item):
     #     print("clickItem")
@@ -151,36 +131,43 @@ class MemoManager(QWidget):
         print(item.text(0), "클릭")
         print(self.folder_treewidget.currentIndex().row())
 
+        listIndex = self.folder_treewidget.currentIndex().row()
+
         self.folder_item = item
         self.folderText = item.text(0)
+        self.file_list[listIndex].folderText = item.text(0)
 
         if item.isSelected() == True:
             self.removefolder.setEnabled(True)
-            self.newfile.setEnabled(True)
+            self.file_list[listIndex].newfile.setEnabled(True)
         else:
             self.removefolder.setEnabled(False)
-            self.newfile.setEnabled(False)
+            self.file_widget.newfile.setEnabled(False)
 
         self.status_text()
 
+        # 스텟에 저장된 위젯을 선택한다(+1 은 임시위젯으로 인해 0번 자리가 채워졌기 때문)
+        self.stackedwidget.setCurrentIndex(self.folder_treewidget.currentIndex().row()+1)
+
     def folder_changedItem(self, item):
         print("changed")
+        listIndex = self.folder_treewidget.currentIndex().row()
 
         self.folder_item = item
         self.folderText = item.text(0)
+        self.file_list[listIndex].folderText = item.text(0)
 
-        file_childCount = self.file_root.childCount()
+        file_childCount = self.file_list[listIndex].file_root.childCount()
 
         for count in range(file_childCount):
-            file_child = self.file_root.child(count)
+            file_child = self.file_list[listIndex].file_root.child(count)
             file_child.setText(3, self.folderText)
-
-        #self.file_root.setText(3, self.folderText)
 
         self.status_text()
 
     def status_text(self):
         self.parent.statusBar().showMessage("선택 목록 : " + self.folderText)
+        
 
         # if(self.newfolder.isEnabled != True):
         #     print("newfolder True")
@@ -196,3 +183,57 @@ class MemoManager(QWidget):
     #     contextmenu = QMenu(self)
     #     new = contextmenu.addAction("New")
     #     action = contextmenu.exec_(self.mapToGlobal(event.pos()))
+
+
+# 파일 위젯
+class File_Widgit(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        print("class : File_Widgit")
+
+        self.widget = QTreeWidget()
+        self.file_root = QTreeWidget.invisibleRootItem(self.widget)
+
+        self.folderText = ""
+
+        self.UI()
+        self.contextMenu()
+
+    def UI(self):
+        self.widget.setHeaderLabels(["제목", "생성일", "수정일", "그룹"])
+        self.widget.setContextMenuPolicy(Qt.ActionsContextMenu)
+        self.widget.setColumnWidth(0, 160)
+        self.widget.setColumnWidth(1, 150)
+        self.widget.setColumnWidth(2, 150)
+
+    def get_widget(self):
+        return self.widget
+
+    # 파일추가
+    def addFile(self, title, date, modify, group):
+        # 트리위젯에 추가할 아이템 생성
+        item = QTreeWidgetItem()
+
+        item.setText(0, title)
+        item.setText(1, date)
+        item.setText(2, modify)
+        item.setText(3, group)
+
+        return item
+
+    def contextMenu(self):
+        self.newfile = QAction("새 파일", self.widget)
+        self.widget.addAction(self.newfile)
+        self.newfile.setEnabled(False)
+
+        self.newfile.triggered.connect(lambda : self.file_contextClick())
+
+    def file_contextClick(self):
+        print("newfile.triggered.connect()")
+
+        day = QDate.currentDate()
+        time = QTime.currentTime()
+
+        self.file_root.addChild(self.addFile("bird 파일", day.toString('yyyy/MM/dd') + " - " + time.toString('hh:mm:ss'), day.toString('yyyy/MM/dd')+ " - " + time.toString('hh:mm:ss'), self.folderText))
+
